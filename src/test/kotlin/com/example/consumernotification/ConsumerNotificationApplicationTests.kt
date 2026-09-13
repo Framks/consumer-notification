64617b8dde07c4d98f334c002fe6c7725c1ae104
Framks <galvesa9@gmail.com>
2026-09-13 00:03:39 -0300

    Init project

---
+++ src/test/kotlin/com/example/consumernotification/ConsumerNotificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.consumernotification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsumerNotificationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
